31b2a5cedc125f5095c33939ec10d8d5599c44c7
Thiago Kachinsky <tjk091@gmail.com>
2021-05-27 19:40:59 -0300

    Adicionando inputs de text ao formulário AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,9 +1,60 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action'
+    }
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { onClick } = this.props;
+    const { title, subtitle } = this.state;
+
     return(
-      <p>ola</p>
+      <form data-testid="add-movie-form">
+        <label htmlFor="add-movie-input-title" data-testid="title-input-label">
+          Título
+          <input
+            data-testid="title-input"
+            name="title"
+            type="text" 
+            id="add-movie-input-title" 
+            value={ title } 
+            onChange={ this.handleChange }
+          />
+        </label>
+
+        <label htmlFor="add-movie-input-subtitle" data-testid="subtitle-input-label">
+        Subtítulo
+          <input
+            data-testid="subtitle-input"
+            name="subtitle"
+            type="text" 
+            id="add-movie-input-subtitle" 
+            value={ subtitle } 
+            onChange={ this.handleChange }
+          />
+        </label>
+      </form>
     )
   }
 }
