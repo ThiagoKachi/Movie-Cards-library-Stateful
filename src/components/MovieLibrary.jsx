35317cb95922ccc3ede7bb915743b240ba767c62
Thiago Kachinsky <tjk091@gmail.com>
2021-05-27 23:32:55 -0300

    Add movieList dentro de MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,9 +1,51 @@
 import React from 'react';
+import SearchBar from './SearchBar';
 
 class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleSearchText = this.handleSearchText.bind(this);
+    this.handleBookmarkedOnly = this.handleBookmarkedOnly.bind(this);
+    this.handleSelectedGenre = this.handleSelectedGenre.bind(this);
+    
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
+    }
+  }
+
+  handleSearchText(event) {
+    this.setState({
+      searchText: event.target.value
+    })
+  }
+
+  handleBookmarkedOnly(event) {
+    this.setState({
+      bookmarkedOnly: event.target.checked
+    })
+  }
+
+  handleSelectedGenre(event) {
+    this.setState({
+      selectedGenre: event.target.value
+    })
+  }
+
   render() {
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
     return(
-      <p>Olá, mundo!</p>
+      <SearchBar 
+        SearchBar={ searchText }
+        bookmarkedOnly={ bookmarkedOnly }
+        selectedGenre={ selectedGenre }
+        onSearchTextChange={ this.handleSearchText }
+        onBookmarkedChange={ this.handleBookmarkedOnly }
+        onSelectedGenreChange={ this.handleSelectedGenre }
+      />
     )
   }
 }
